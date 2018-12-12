148f8103a6379bb7a042dbaad87f1daf1cc7a96c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 09:13:24 +0100

    Modifica del file convertendo un numero in una base richiesta

--- Convertitore/Program.cs
+++ Convertitore/Program.cs
@@ -14,14 +14,24 @@ namespace Convertitore
             int num = 0;
             Console.WriteLine("inserisci un numero da convertire in binario");
             num = Convert.ToInt32(Console.ReadLine());
+
+            int valbase;
+            Console.WriteLine("inserisci una base per convertire il numero");
+            do
+            {
+                valbase = Convert.ToInt32(Console.ReadLine());
+
+            } while (valbase != 2 && valbase != 8 && valbase != 16);
+
             while (num > 1)
             {
-                int resto = num % 2;
+                int resto = num % valbase;
                 risultato = Convert.ToString(resto) + risultato;
-                num = num / 2;
+                num = num / valbase;
             }
+
             risultato = Convert.ToString(num) + risultato;
-            Console.WriteLine($"il numero binario è {risultato}");
+            Console.WriteLine($"il numero convertito è {risultato}");
 
             Console.ReadLine();
         }
