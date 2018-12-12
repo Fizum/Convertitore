a43e2362bc53ba0bbbb43abc0410df8b4fae8ff3
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 09:01:31 +0100

    prima versione non funzionante

--- Convertitore/Program.cs
+++ Convertitore/Program.cs
@@ -23,6 +23,29 @@ namespace Convertitore
             risultato = Convert.ToString(num) + risultato;
             Console.WriteLine($"il numero binario è {risultato}");
 
+            Console.WriteLine();
+
+
+            int valbase = 0;
+            Console.WriteLine("scegli una base tra 8 e 16 da convertire il numero");
+            do
+            {
+                valbase = Convert.ToInt32(Console.ReadLine());
+                if (valbase != 8 && valbase != 16)
+                {
+                    Console.WriteLine("reinserisci una base scegliendo tra 8 e 16");
+                }
+            } while (valbase != 8 && valbase != 16);
+
+            risultato = "";
+            num = 0;
+            while (num > 0)
+            {
+                int resto = num % valbase;
+                risultato = Convert.ToString(resto, valbase) + risultato;
+                num = num / valbase;
+            }
+            Console.WriteLine($"il numero convertito in base {valbase} è {risultato}");
             Console.ReadLine();
         }
     }
